f5aa29e884117c0b5ed0ef98400c669526eee129
Max Edwards <youwontforgetthis@gmail.com>
2023-06-10 14:07:36 +0100

    added retry func

---
+++ pkg/tools/retry.go
@@ -0,0 +1,35 @@
+package tools
+
+import (
+	"time"
+
+	"github.com/cockroachdb/errors"
+	"github.com/rs/zerolog/log"
+)
+
+type NoRetryError struct{}
+
+func (nre NoRetryError) Error() string {
+	return "Fundamental problem, skipping retries"
+}
+
+func Retry(operation func() error, delay time.Duration, maxWait time.Duration) error {
+	var totalWaited time.Duration
+	for {
+		if totalWaited > maxWait {
+			return errors.New("Exceeded maximum wait period")
+		}
+		err := operation()
+		var noRetry NoRetryError
+		if errors.As(err, &noRetry) {
+			return err
+		}
+		if err == nil {
+			break
+		}
+		log.Info().Msg("Retry...")
+		time.Sleep(delay)
+		totalWaited += delay
+	}
+	return nil
+}
