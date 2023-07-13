b361a93d69e15a11ce8ec5d7fb339e155e024112
Max Edwards <youwontforgetthis@gmail.com>
2023-07-13 17:23:43 +0100

    Added test for new address for cln client

--- clients/cln/api_test.go
+++ clients/cln/api_test.go
@@ -31,3 +31,21 @@ func TestHandleWalletBalance(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Contains(t, string(bodyBytes), "21")
 }
+
+func TestHandleNewAddress(t *testing.T) {
+	mockClient := mocks.NewNodeClient(t)
+
+	// mock based test for the handleGetNewAddress function
+	address := "bcrt1qddzehdyj5e7w4sfya3h9qznnm80etc9gkpk0qd"
+	mockClient.On("NewAddr", mock.Anything, mock.Anything).
+		Return(&clnGRPC.NewaddrResponse{Bech32: &address}, nil)
+
+	req := httptest.NewRequest(http.MethodPost, "/", nil)
+	res := httptest.NewRecorder()
+
+	handleNewAddress(res, req, mockClient)
+
+	bodyBytes, err := io.ReadAll(res.Result().Body)
+	assert.Nil(t, err)
+	assert.Contains(t, string(bodyBytes), address)
+}
