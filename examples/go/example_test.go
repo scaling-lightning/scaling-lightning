1a5357fe6b50e6785d8946fae75deeafea53d933
Max Edwards <youwontforgetthis@gmail.com>
2023-07-18 15:06:22 +0100

    added retry logic to the example test

--- examples/go/example_test.go
+++ examples/go/example_test.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	sl "github.com/scaling-lightning/scaling-lightning/pkg/network"
+	"github.com/scaling-lightning/scaling-lightning/pkg/tools"
 	"github.com/stretchr/testify/assert"
 )
 
+// will need a longish (few mins) timeout
 func TestMain(t *testing.T) {
 	assert := assert.New(t)
+
 	err := sl.StartViaHelmfile("../helmfiles/helmfile.yaml")
 	assert.NoError(err)
 	defer sl.StopViaHelmfile("../helmfiles/helmfile.yaml")
 
-	err = sl.Send("bitcoind", "cln1", 1_000_000)
+	// this one will take a little while as the network is starting up
+	err = tools.Retry(func() error {
+		return sl.Send("bitcoind", "cln1", 1_000_000)
+	}, time.Second*15, time.Minute*2)
+
 	assert.NoError(err)
 
 	// log.Fatal().Msg("Testing main")
