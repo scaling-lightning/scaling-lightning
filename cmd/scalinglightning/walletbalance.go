deda4ba3ac00d24f4aa80150f3855bd98301b77c
Max Edwards <youwontforgetthis@gmail.com>
2023-07-19 16:11:25 +0100

    working wallet balance command

--- cmd/scalinglightning/walletbalance.go
+++ cmd/scalinglightning/walletbalance.go
@@ -3,34 +3,30 @@ package scalinglightning
 import (
 	"fmt"
 
+	sl "github.com/scaling-lightning/scaling-lightning/pkg/network"
 	"github.com/spf13/cobra"
 )
 
-// walletbalanceCmd represents the walletbalance command
+var balanceNodeName string
+
 var walletbalanceCmd = &cobra.Command{
 	Use:   "walletbalance",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Get the onchain wallet balance of a node",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("walletbalance called")
+		balance, err := sl.GetWalletBalanceSats(balanceNodeName)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println(balance)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(walletbalanceCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	walletbalanceCmd.PersistentFlags().String("node", "", "The name of the node to get the wallet balance of")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// walletbalanceCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	walletbalanceCmd.Flags().
+		StringVarP(&balanceNodeName, "node", "n", "", "The name of the node to get the wallet balance of")
+	walletbalanceCmd.MarkFlagRequired("node")
 }
