c38d66954b2afe9c61935ad3dbabbe2dc1c681a2
Max Edwards <youwontforgetthis@gmail.com>
2023-07-19 16:11:38 +0100

    working send command

--- cmd/scalinglightning/send.go
+++ cmd/scalinglightning/send.go
@@ -1,40 +1,40 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package scalinglightning
 
 import (
 	"fmt"
 
+	sl "github.com/scaling-lightning/scaling-lightning/pkg/network"
 	"github.com/spf13/cobra"
 )
 
-// sendCmd represents the send command
+var sendFromName string
+var sendToName string
+var sendAmount uint64
+
 var sendCmd = &cobra.Command{
 	Use:   "send",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Send on chain funds betwen nodes",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("send called")
+		err := sl.Send(sendFromName, sendToName, sendAmount)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println("Sent funds")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(sendCmd)
 
-	// Here you will define your flags and configuration settings.
+	sendCmd.Flags().StringVarP(&sendFromName, "from", "f", "", "Name of node to send from")
+	sendCmd.MarkFlagRequired("from")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// sendCmd.PersistentFlags().String("foo", "", "A help for foo")
+	sendCmd.Flags().StringVarP(&sendToName, "to", "t", "", "Name of node to send to")
+	sendCmd.MarkFlagRequired("to")
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// sendCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	sendCmd.Flags().Uint64VarP(&sendAmount, "amount", "a", 0, "Amount of satoshis to send")
+	sendCmd.MarkFlagRequired("amount")
+
 }
