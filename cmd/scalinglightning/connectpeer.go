718bcda9445bbb943ef9d9a08b213aef4ab7a468
Max Edwards <youwontforgetthis@gmail.com>
2023-07-19 16:12:45 +0100

    working connect peer command

--- cmd/scalinglightning/connectpeer.go
+++ cmd/scalinglightning/connectpeer.go
@@ -1,40 +1,37 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package scalinglightning
 
 import (
 	"fmt"
 
+	sl "github.com/scaling-lightning/scaling-lightning/pkg/network"
 	"github.com/spf13/cobra"
 )
 
-// connectpeerCmd represents the connectpeer command
+var connectpeerFromName string
+var connectpeerToName string
+
 var connectpeerCmd = &cobra.Command{
 	Use:   "connectpeer",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Connect peers",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("connectpeer called")
+		err := sl.ConnectPeer(connectpeerFromName, connectpeerToName)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println("Connect peer command received")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(connectpeerCmd)
 
-	// Here you will define your flags and configuration settings.
+	connectpeerCmd.Flags().
+		StringVarP(&connectpeerFromName, "from", "f", "", "Name of the node to connect from")
+	connectpeerCmd.MarkFlagRequired("from")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// connectpeerCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// connectpeerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	connectpeerCmd.Flags().
+		StringVarP(&connectpeerToName, "to", "t", "", "Name of the node to connect from")
+	connectpeerCmd.MarkFlagRequired("from")
 }
