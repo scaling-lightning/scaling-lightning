622440a959f3b3b2f219149d4f075fc700b7604b
Max Edwards <youwontforgetthis@gmail.com>
2023-10-03 19:00:16 +0100

    fixed bitcoinnode test

--- pkg/bitcoinnode/bitcoinnode_test.go
+++ pkg/bitcoinnode/bitcoinnode_test.go
@@ -3,15 +3,20 @@ package bitcoinnode
 import (
 	"testing"
 
+	"github.com/scaling-lightning/scaling-lightning/pkg/standardclient/common"
 	"github.com/scaling-lightning/scaling-lightning/pkg/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestBitcoinNode(t *testing.T) {
 	assert := assert.New(t)
 
 	bitcoinNode := &BitcoinNode{}
-	response, err := bitcoinNode.SendToAddress(nil, "address", types.NewAmountSats(1000))
+
+	mockGrpcClient := common.NewMockCommonClient(t)
+	mockGrpcClient.On("Send", mock.Anything, mock.Anything).Return(&common.SendResponse{}, nil)
+	response, err := bitcoinNode.SendToAddress(mockGrpcClient, "address", types.NewAmountSats(1000))
 	assert.Nil(err)
 	assert.NotNil(response)
 }
